06d016034cdc189b59bca09895d30432531c5563
Felipe Sabogal <csabogal@grupodot.com>
2023-10-24 11:00:17 -0500

    lista de productos array js

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ function toogleDesktopMenu(){
     if(!isAsideOClosed){
         asideCarrito.classList.add('inactive');
     }
-    
+
     desktopMenu.classList.toggle('inactive');
     mobileMenu.classList.add('inactive');
 }
@@ -50,3 +50,58 @@ function toogleCarritoAside(){
 
     asideCarrito.classList.toggle('inactive');
 }
+
+//productos
+const cardsContainer = document.querySelector('.cards-container');
+
+const producList = [];
+producList.push({
+    name: 'Bike',
+    price: '120.000',
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+});
+producList.push({
+    name: 'Bike',
+    price: '120.000',
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+});
+producList.push({
+    name: 'Bike',
+    price: '120.000',
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+});
+
+function renderProduct(arr){
+    for(product of arr){
+
+        const productCard = document.createElement('div');
+        productCard.classList.add('product-card');
+    
+        const productImg = document.createElement('img');
+        productImg.setAttribute('src', product.image);
+    
+        const productInfo = document.createElement('div');
+        productInfo.classList.add('product-info');
+    
+        const productInfoDiv = document.createElement('div');
+    
+        const productPrice = document.createElement('p');
+        productPrice.innerText = '$' + product.price;
+    
+        const productName = document.createElement('p');
+        productName.innerText = product.name;
+    
+        const productInfoFigure = document.createElement('figure');
+        const productImgCart = document.createElement('img');
+        productImgCart.setAttribute('src', './icons/bt_add_to_cart.svg');
+    
+        productInfoDiv.append(productPrice, productName);
+        productInfoFigure.appendChild(productImgCart);
+        productInfo.append(productInfoDiv, productInfoFigure);
+        productCard.append(productImg, productInfo);
+
+        cardsContainer.appendChild(productCard);
+    }
+}
+
+renderProduct(producList);
