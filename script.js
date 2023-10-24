4f6d1cb6cc1c4e6cabd91ba4edcf832ddcb8c719
Felipe Sabogal <csabogal@grupodot.com>
2023-10-24 10:03:17 -0500

    agregar y arreglar carrito de compras

--- script.js
+++ script.js
@@ -5,7 +5,15 @@ const desktopMenu = document.querySelector('.desktop-menu');
 menuEmail.addEventListener('click', toogleDesktopMenu);
 
 function toogleDesktopMenu(){
+
+    const isAsideOClosed = asideCarrito.classList.contains('inactive');
+
+    if(!isAsideOClosed){
+        asideCarrito.classList.add('inactive');
+    }
+    
     desktopMenu.classList.toggle('inactive');
+    mobileMenu.classList.add('inactive');
 }
 
 //Funcion del menu mobile
@@ -15,6 +23,30 @@ const mobileMenu = document.querySelector('.mobile-menu');
 HamIconMenu.addEventListener('click', toogleMobileMenu);
 
 function toogleMobileMenu(){
+
+    const isAsideOClosed = asideCarrito.classList.contains('inactive');
+
+    if(!isAsideOClosed){
+        asideCarrito.classList.add('inactive');
+    }
+
     mobileMenu.classList.toggle('inactive');
+    desktopMenu.classList.add('inactive');
 }
 
+//funcion carrito
+const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
+const asideCarrito = document.querySelector('.product-detail');
+
+menuCarritoIcon.addEventListener('click', toogleCarritoAside);
+
+function toogleCarritoAside(){
+
+    const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
+
+    if(!isMobileMenuClosed){
+        mobileMenu.classList.add('inactive');
+    }
+
+    asideCarrito.classList.toggle('inactive');
+}
